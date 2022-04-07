51de54127624d51a6999bd0a9fac8feaae1f8b41
AndrosDe <andreas_beier1981@yahoo.de>
2022-04-07 07:51:44 +0000

    Introduction comment added to the top of the run.py

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# WELLCOME TO "THE SHUT THE BOX" DICE-GAME PYTHON CODE
+# Shut the box (also called canoga, batten down the hatches or trick-track)
+# is a game of dice for one or more players.
+# Traditionally, a counting box is used with tiles numbered 1 to 9.
+# More can information on the game can be found here:
+# https://en.wikipedia.org/wiki/Shut_the_box
